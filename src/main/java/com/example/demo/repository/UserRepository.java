81a33729038abfb59330ea6dea0050eb05db9d66
hjhng125 <hjhng125@nate.com>
2020-07-20 12:49:32 +0900

    add user

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 
 import com.example.demo.model.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByEmail(String email);
 }
